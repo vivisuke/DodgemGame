68e22dd3a5637bef024d6ad49a150663e646ec58
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2025-01-11 08:49:27 +0900

    0004 ◎ C 01/11 01/11 0.001│ToDo│車初期配置

--- dodgemgame/scripts/board_rect.gd
+++ dodgemgame/scripts/board_rect.gd
@@ -12,7 +12,15 @@ const Y0 = CELL_WIDTH / 2
 const BD_WD = CELL_WIDTH * N_CELLS
 const BD_HT = CELL_WIDTH * N_CELLS
 
+func xyToPos(x, y):
+	var px = X0 + (x+0.5) * CELL_WIDTH
+	var py = X0 + (N_CELLS-0.5-y) * CELL_WIDTH
+	return Vector2(px, py)
 func _ready():
+	$RedCar1.position = xyToPos(1, 0)
+	$RedCar2.position = xyToPos(2, 0)
+	$BlueCar1.position = xyToPos(0, 1)
+	$BlueCar2.position = xyToPos(0, 2)
 	pass
 func _draw():
 	draw_line(Vector2(X0, Y0), Vector2(X0+BD_WD, Y0), Color.BLACK, 2.0)
