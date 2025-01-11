738dee97969deaa6e993fba1a8d1845325ce6e02
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2025-01-12 07:26:26 +0900

    0007 ◎ C 01/11 01/12 0.001│ToDo│車管理配列追加

--- dodgemgame/scripts/Board.gd
+++ dodgemgame/scripts/Board.gd
@@ -8,6 +8,8 @@ const RED_CAR = -1
 var m_width
 var m_height
 var m_cells = []			# ２次元配列、m_cells[y][x] で参照
+var m_red_cars = PackedVector2Array()		# 赤車位置 (x, y)
+var m_blue_cars = PackedVector2Array()
 
 func _init(wd = 3, ht = 3):		# 横・縦サイズ
 	init_board(wd, ht)
@@ -23,9 +25,17 @@ func init_board(wd, ht):
 	for i in range(1, m_width):
 		m_cells[0][i] = RED_CAR
 		m_cells[i][0] = BLUE_CAR
+	m_red_cars.resize(2)
+	m_red_cars[0] = Vector2(1, 0)
+	m_red_cars[1] = Vector2(2, 0)
+	m_blue_cars.resize(2)
+	m_blue_cars[0] = Vector2(0, 1)
+	m_blue_cars[1] = Vector2(0, 2)
 func print():
 	for v in range(m_height-1, -1, -1):
 		print(m_cells[v])
+	print("red: ", m_red_cars)
+	print("blue: ", m_blue_cars)
 func _ready():
 	pass # Replace with function body.
 func _process(delta):
